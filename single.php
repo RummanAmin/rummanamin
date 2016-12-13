c7f70f5b869ef2d2dad7361f9416666f5bc35531
Rumman Amin <Rumman.amin10@gmail.com>
2016-12-13 01:13:20 +0000

    Inline Newsletter
    Add custom built inline newsletter to blog posts.

--- single.php
+++ single.php
@@ -8,6 +8,32 @@
     <section class="layout">
       <article class="post-content">
         <?php the_content(); ?>
+        <div id="inline-newsletter">
+          <div class="inline-newsimage">
+            <img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/backgrounds/Image10.jpg" alt="">
+          </div>
+          <!-- Begin MailChimp Signup Form -->
+          <div id="mc_embed_signup">
+            <form action="//rummanamin.us7.list-manage.com/subscribe/post?u=ba090bbe727834c5b13f46e0f&amp;id=f2c530e56d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+              <div id="mc_embed_signup_scroll">
+              	<h4>Sign up for Updates</h4>
+                <p class="newsletter-message">Subscribe to the Monthly Newsletter for latest posts, FREE Resources and updates my eBook.</p>
+                <div class="mc-field-group">
+                	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+                </label>
+                	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                </div>
+              	<div id="mce-responses" class="clear">
+              		<div class="response" id="mce-error-response" style="display:none"></div>
+              		<div class="response" id="mce-success-response" style="display:none"></div>
+              	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ba090bbe727834c5b13f46e0f_f2c530e56d" tabindex="-1" value=""></div>
+                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+              </div>
+            </form>
+          </div>
+          <!--End mc_embed_signup-->
+        </div>
         <div id="author-box">
           <div class="author-main">
             <article class="author-img">
