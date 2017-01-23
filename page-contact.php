f43af67cbebef34a1819eafc01090c2770c4cc60
Rumman Amin <Rumman.amin10@gmail.com>
2017-01-23 21:38:28 +0000

    Update IG link

--- page-contact.php
+++ page-contact.php
@@ -21,7 +21,7 @@
         <p>Follow me on social media to get all the latest updates.</p>
         <div class="social-links">
           <ul>
-            <li><a href="https://www.instagram.com/bytebubble" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/icons/instagram.svg" alt="Instagram"></a></li>
+            <li><a href="https://www.instagram.com/rumman.amin" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/icons/instagram.svg" alt="Instagram"></a></li>
             <li><a href="https://www.twitter.com/rumantweets" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/icons/twitter.svg" alt="Twitter"></a></li>
             <li><a href="https://www.facebook.com/ruman.amin" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/icons/facebook.svg" alt="Facebook"></a></li>
             <li><a href="https://uk.linkedin.com/in/rummanamin" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/icons/linkedin.svg" alt="LinkedIn"></a></li>
