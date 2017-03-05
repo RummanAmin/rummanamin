c4c28d81a3fe430fee053dd006dec983e5f85e26
Rumman Amin <Rumman.amin10@gmail.com>
2017-03-05 21:21:09 +0000

    Update Social Media Links

--- 404.php
+++ 404.php
@@ -56,10 +56,10 @@
         <h4 class="widget-title">Social Media</h4>
         <div class="social-links">
           <ul>
-            <li><a href="https://www.instagram.com/bytebubble" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/icons/instagram.svg" alt="Instagram"></a></li>
-            <li><a href="https://www.twitter.com/rumantweets" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/icons/twitter.svg" alt="Twitter"></a></li>
-            <li><a href="https://www.facebook.com/ruman.amin" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/icons/facebook.svg" alt="Facebook"></a></li>
+            <li><a href="https://www.instagram.com/rumman.amin" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/icons/instagram.svg" alt="Instagram"></a></li>
+            <li><a href="https://medium.com/@rumman" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/icons/medium.svg" alt="Medium"></a></li>
             <li><a href="https://uk.linkedin.com/in/rummanamin" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/icons/linkedin.svg" alt="LinkedIn"></a></li>
+            <li><a href="https://www.twitter.com/rumantweets" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/icons/twitter.svg" alt="Twitter"></a></li>
           </ul>
         </div>
       </article>
