1a1657e6d5cd4e1e2d7d1151f2bbb4d1d0422f28
Rumman Amin <Rumman.amin10@gmail.com>
2017-03-05 21:21:21 +0000

    Add Adsense
    Bottom of blog posts

--- single.php
+++ single.php
@@ -27,24 +27,27 @@
 
         <article class="post-split">
           <div id="post-main"><?php the_content(); ?></div>
+          <div class="insert-post-ads">            
+            <?php get_template_part( 'content', 'ad' ); ?>
+          </div>
           <div class="share-post">
-                      <h4>Share this Post</h4>
-                      <p>Thank you for reading this post. Please help me by sharing it with your friends!</p>
-                      <ul class="share-links">
-                        <li>
-                          <a href="http://twitter.com/share?text=<?php echo urlencode(the_title()); ?>&url=<?php echo urlencode(the_permalink()); ?>&via=rumantweets&related=<?php echo urlencode("Digital Project Management, Web Design, Web Development"); ?>" title="Share on Twitter" rel="nofollow" target="_blank" class="link-tw">Twitter</a>
-                        </li>
-                        <li>
-                          <a target="_blank" href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&t=<?php the_title(); ?>" class="link-fb">Facebook</a>
-                        </li>
-                        <li>
-                          <a target="_blank" href="https://plus.google.com/share?url=<?php the_permalink();?>&t=<?php the_title(); ?>" class="link-gp">Google +</a>
-                        </li>
-                        <li>
-                          <a target="_blank" href="https://www.tumblr.com/share?v=3&u=<?php the_permalink();?>&t=<?php the_title(); ?>" class="link-tm">Tumblr</a>
-                        </li>
-                      </ul>
-                    </div>
+            <h4>Share this Post</h4>
+            <p>Thank you for reading this post. Please help me by sharing it with your friends!</p>
+            <ul class="share-links">
+              <li>
+                <a href="http://twitter.com/share?text=<?php echo urlencode(the_title()); ?>&url=<?php echo urlencode(the_permalink()); ?>&via=rumantweets&related=<?php echo urlencode("Digital Project Management, Web Design, Web Development"); ?>" title="Share on Twitter" rel="nofollow" target="_blank" class="link-tw">Twitter</a>
+              </li>
+              <li>
+                <a target="_blank" href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&t=<?php the_title(); ?>" class="link-fb">Facebook</a>
+              </li>
+              <li>
+                <a target="_blank" href="https://plus.google.com/share?url=<?php the_permalink();?>&t=<?php the_title(); ?>" class="link-gp">Google +</a>
+              </li>
+              <li>
+                <a target="_blank" href="https://www.tumblr.com/share?v=3&u=<?php the_permalink();?>&t=<?php the_title(); ?>" class="link-tm">Tumblr</a>
+              </li>
+            </ul>
+          </div>
           <div id="inline-newsletter">
             <?php get_template_part( 'content', 'background' ); ?>
             <!-- Begin MailChimp Signup Form -->
