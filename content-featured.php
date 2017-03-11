03ed103b72935ec03f505a58089914e40fc398be
Rumman Amin <Rumman.amin10@gmail.com>
2017-03-11 13:30:26 +0000

    Mobile Detection
    Differentiate between blog and resources

--- content-featured.php
+++ content-featured.php
@@ -4,17 +4,35 @@ $detect = new Mobile_Detect;
 
 ?>
 
-<?php if ( !$detect->isMobile() ): ?>
+<?php if ((!$detect->isMobile()) ): ?>
 
-<div class="band poster">
-  <section class="layout">
-    <article>
-      <?php $the_query = new WP_Query( 'showposts=1&cat=-6' ); ?>
-      <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail();?></a>
-      <?php endwhile; wp_reset_postdata(); ?>
-    </article>
-  </section>
-</div>
+
+  <?php if (is_page( 16 )): ?>
+
+    <div class="band poster">
+      <section class="layout">
+        <article>
+          <?php $the_query = new WP_Query( 'category_name=resources&showposts=1' ); ?>
+          <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail();?></a>
+          <?php endwhile; wp_reset_postdata(); ?>
+        </article>
+      </section>
+    </div>
+
+  <?php else: ?>
+
+    <div class="band poster">
+      <section class="layout">
+        <article>
+          <?php $the_query = new WP_Query( 'showposts=1&cat=-6' ); ?>
+          <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail();?></a>
+          <?php endwhile; wp_reset_postdata(); ?>
+        </article>
+      </section>
+    </div>
+
+  <?php endif; ?>
 
 <?php endif; ?>
