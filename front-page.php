1292222fde200dfc0fe71d4cd7b44220d647def5
Rumman Amin <Rumman.amin10@gmail.com>
2017-03-19 23:48:48 +0000

    New Homepage

--- front-page.php
+++ front-page.php
@@ -1,11 +1,115 @@
+<?php $detect = new Mobile_Detect; ?>
+
 <?php get_header(); ?>
 
   <?php get_template_part('content', 'background-intro'); ?>
 
+  <div class="band main first">
+    <section class="layout">
+      <!-- <h2>Latest Posts</h2>
+      <h4 class="section-intro">Occasionally write about Project Management, Design, Development and share the odd post or two about everyday life. Enjoy!</h4> -->
+      <ul class="list-grid">
+        <?php
+          $args = array(
+          'post_type' => 'post',
+          'posts_per_page' => 2
+          );
+          $query = new WP_Query( $args );
+          while ($query->have_posts()) : $query->the_post();
+        ?>
+          <li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('post-thumb');?></a>
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <h5><?php the_category( ', ' ); ?> | <?php the_date(); ?></h5>
+            <?php the_excerpt(); ?>
+            <a href="<?php the_permalink(); ?>" class="button">Read more</a>
+          </li>
+        <?php endwhile; wp_reset_postdata(); ?>
+        <li>
+          <?php if (!$detect->isMobile() || $detect->isTablet() ): ?>
+            <ul class="simple-post-list">
+            <?php
+              $args = array(
+              'post_type' => 'post',
+              'posts_per_page' => 5,
+              'offset' => 2
+              );
+              $query = new WP_Query( $args );
+              while ($query->have_posts()) : $query->the_post();
+            ?>
+              <li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                <h5 id="remove-header"><?php the_category( ', ' ); ?> | <?php the_date(); ?></h5>
+
+              </li>
+            <?php endwhile; wp_reset_postdata(); ?>
+          </ul>
+          <?php endif; ?>
+        </li>
+      </ul>
+    </section>
+  </div>
+
+  <div class="band home-ebook">
+    <section class="layout">
+        <?php
+        $args = array(
+        'post_type' => 'ebook',
+        'posts_per_page' => 1
+        );
+        $query = new WP_Query( $args );
+        while ($query->have_posts()) : $query->the_post();
+      ?>
+        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <article>
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <h5><?php the_category( ', ' ); ?> | <?php the_date(); ?></h5>
+            <?php the_excerpt(); ?>
+            <a href="<?php the_permalink(); ?>" class="button">Download</a>
+          </article>
+
+          <article>
+            <a href="<?php the_permalink(); ?>"><img src="<?php the_field('cover'); ?>" class="book-cover" /></a>
+          </article>
+        </div>
+      <?php endwhile; wp_reset_postdata(); ?>
+    </section>
+  </div>
+
+  <div class="band floating-box">
+    <section class="layout">
+      <article>
+        <!-- Begin MailChimp Signup Form -->
+        <div id="mc_embed_signup">
+          <form action="//rummanamin.us7.list-manage.com/subscribe/post?u=ba090bbe727834c5b13f46e0f&amp;id=f2c530e56d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+              <h4>Sign up for Updates</h4>
+              <p class="newsletter-message">Subscribe to the Monthly Newsletter for latest posts, FREE Resources and updates on my eBook.</p>
+              <div class="mc-field-group">
+                <input type="email" value="" placeholder="Email Address" name="EMAIL" class="required email" id="mce-EMAIL">
+              </div>
+              <div id="mce-responses" class="clear">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+              </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+              <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ba090bbe727834c5b13f46e0f_f2c530e56d" tabindex="-1" value=""></div>
+              <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+            </div>
+          </form>
+        </div>
+        <!--End mc_embed_signup-->
+      </article>
+      <article></article>
+    </section>
+  </div>
+
   <div class="band projects">
     <section class="layout">
+      <!-- <h2>Recent Projects</h2>
+      <h4 class="section-intro">My job is to try and make things simpler, smarter and more engaging. Whether that’s through design, development or project management.</h4> -->
       <ul class="list-grid">
-        <?php $the_query = new WP_Query( 'showposts=3&post_type=project' ); ?>
+        <?php $the_query = new WP_Query( 'showposts=1&post_type=project' ); ?>
         <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
           <li class="move-right">
             <article>
@@ -22,23 +126,5 @@
       </ul>
     </section>
   </div>
-
-  <div class="band main">
-    <section class="layout">
-      <ul class="list-grid">
-        <?php $the_query = new WP_Query( 'showposts=3' ); ?>
-          <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-          <li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('post-thumb');?></a>
-            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-            <h5><?php the_category( ', ' ); ?> | <?php the_date(); ?></h5>
-            <?php the_excerpt(); ?>
-            <a href="<?php the_permalink(); ?>" class="button">Read more</a>
-          </li>
-        <?php endwhile; wp_reset_postdata(); ?>
-      </ul>
-    </section>
-  </div>
-
 
 <?php get_footer(); ?>
